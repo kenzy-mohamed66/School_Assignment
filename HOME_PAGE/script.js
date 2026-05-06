3e46edae9ebac0aa951a06d4a595d777a0e143c9
sondososama70 <sondososamamahmoud@gmail.com>
2026-05-07 01:06:01 +0300

    handling backend

---
+++ HOME_PAGE/script.js
@@ -0,0 +1,18 @@
+// fetch("http://127.0.0.1:8000/api/tasks/")
+//   .then(response => response.json())
+//   .then(data => {
+//     const container = document.getElementById("tasks-container");
+
+//     data.forEach(task => {
+//       const div = document.createElement("div");
+
+//       div.innerHTML = `
+//         <h3>${task.title}</h3>
+//         <p>${task.description}</p>
+//         <hr>
+//       `;
+
+//       container.appendChild(div);
+//     });
+//   })
+//   .catch(error => console.error("Error:", error));
