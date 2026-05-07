71305175ecde8dceb9b05e395d6b743a2c44a84f
mariam-mohamed225 <11410120240580@stud.cu.edu.eg>
2026-05-07 13:13:56 +0300

    Editing Backend Flow

--- SignUp_Login/accounts/serializers.py
+++ SignUp_Login/accounts/serializers.py
@@ -5,3 +5,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'role']
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters.")
+        return value
+
+    def validate_username(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Username must be at least 3 characters.")
+        return value
--- SignUp_Login/accounts/views.py
+++ SignUp_Login/accounts/views.py
@@ -5,13 +5,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
+import bcrypt
+
 
 class SignupView(APIView):
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        data = request.data.copy()
+
+        # Hash the password before saving
+        password = data.get('password', '')
+        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        data['password'] = hashed.decode('utf-8')
+
+        serializer = UserSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'User created!'}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'User created'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(APIView):
@@ -19,11 +28,18 @@ class LoginView(APIView):
         username = request.data.get('username')
         password = request.data.get('password')
         try:
-            user = User.objects.get(username=username, password=password)
-            return Response({
-                'message': 'Login successful!',
-                'user': user.username,
-                'role': user.role        # send role back to JS
-            })
+            user = User.objects.get(username=username)
+
+            # Check hashed password
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return Response({
+                    'message': 'Login successful!',
+                    'user': user.username,
+                    'role': user.role
+                })
+            
+            else:
+                raise User.DoesNotExist
+            
         except User.DoesNotExist:
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
