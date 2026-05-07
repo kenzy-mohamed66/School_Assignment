7893c4d12c07177e0a96cab7f1427dc9f9d0421d
kenzy2006 <kenzyshalaby2006@gmail.com>
2026-05-07 14:50:11 +0300

    deleting extra folders

--- backend k/myproject/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('data/',            views.get_data,     name='get_data'),
-    path('data/<str:subject>/', views.update_task, name='update_task'),
-]
